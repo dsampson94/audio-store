de30f071ac09e6faaa6a80f5d5a028c65e3b7948
dsampson94 <davesampson15@gmail.com>
2024-05-21 18:55:38 +0200

    code cleanup

--- apps/backend/src/lib/db.ts
+++ apps/backend/src/lib/db.ts
@@ -4,12 +4,7 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-pool.on('connect', () => {
-  console.log('Connected to the database.');
-});
-
-pool.on('error', (err) => {
-  console.error('Database connection error:', err.stack);
-});
+pool.on('connect', () => console.log('Connected to the database.'));
+pool.on('error', (err) => console.error('Database connection error:', err.stack));
 
 export default pool;
